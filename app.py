b245d8764221745ec4ef122dbd839da70fbe97d4
fickmann <fickmann@protonmail.com>
2021-04-22 03:36:25 +0300

    Created 'Close' button.

--- app.py
+++ app.py
@@ -12,10 +12,12 @@ fr_buttons = tk.Frame(window)
 btn_open = tk.Button(fr_buttons, text="Open")
 btn_save = tk.Button(fr_buttons, text="Save")
 btn_save_as = tk.Button(fr_buttons, text="Save As")
+close = tk.Button(fr_buttons, text="Close")
 
 btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
 btn_save.grid(row=1, column=0, sticky="ew", padx=5)
 btn_save_as.grid(row=1, column=1, sticky="ew", padx=5)
+close.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
  
 fr_buttons.grid(row=0, column=0, sticky="ns")
 txt_edit.grid(row=0, column=1, sticky="nsew")
