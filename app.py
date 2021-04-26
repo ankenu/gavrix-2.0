4f87d6a2349a77063dc6936bf39a8f4f46cebfc0
WarNachinka <warnachinka@yandex.ru>
2021-04-27 01:12:50 +0300

    Added scale option menu

--- app.py
+++ app.py
@@ -33,19 +33,34 @@ window.title("Gavrix")
  
 window.rowconfigure(0, minsize=640, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
- 
+
 txt_edit = tk.Text(window, undo=True, font='fixed', borderwidth=2, relief='groove')
 fr_buttons = tk.Frame(window)
+
+scale_option_list = [
+"25%",
+"50%",
+"75%",
+"100%",
+"125%"
+]
+
+scale_option=tk.StringVar(window)
+scale_option.set(scale_option_list[3])
 
 btn_open = tk.Button(fr_buttons, text="Open", command = file_open)
 btn_save = tk.Button(fr_buttons, text="Save")
 btn_save_as = tk.Button(fr_buttons, text="Save As", command = save_as)
 close = tk.Button(fr_buttons, text="Close")
 
+scale = tk.OptionMenu(fr_buttons, scale_option, *scale_option_list)
+scale.config(width=1)
+
 btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
 btn_save.grid(row=1, column=0, sticky="ew", padx=5)
 btn_save_as.grid(row=1, column=1, sticky="ew", padx=5)
 close.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
+scale.grid(row=2, column=1, sticky="ew", padx=5, pady=5)
  
 fr_buttons.grid(row=0, column=0, sticky="ns")
 txt_edit.grid(row=0, column=1, sticky="nsew")
